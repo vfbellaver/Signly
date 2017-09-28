c908928622e91a605202a065979e8d3d6e40eedc
rodrigoslcdevshop <rodrigcursino@gmail.com>
2017-09-28 09:36:09 -0400

    #17 Css in home page and Box component

--- resources/assets/js/components/map/main-map.vue
+++ resources/assets/js/components/map/main-map.vue
@@ -4,7 +4,7 @@
 
             :center="center"
             :zoom="10"
-            style="width: 100vmax; height: 90vmin;"
+            style="width: 100%; height: 90vmin;"
 
     >
             <gmap-info-window :options="infoOptions" :position="infoWindowPos" :opened="infoWinOpen" @closeclick="infoWinOpen=false">
--- resources/assets/js/components/shared/Boxes/Box.vue
+++ resources/assets/js/components/shared/Boxes/Box.vue
@@ -1,3 +1,6 @@
+
+
+
 <template>
     <div class="ibox float-e-margins">
         <slot></slot>
