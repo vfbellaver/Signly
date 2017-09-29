fbede288280293b7fdc5f9b5deb9c51c7b614591
Vagner slcdevshop <vfbellaver@outlook.com>
2017-09-29 08:21:28 -0400

    #16 delete menu nav Import CsvFile

--- resources/views/layouts/_menu.blade.php
+++ resources/views/layouts/_menu.blade.php
@@ -12,9 +12,6 @@
 <li class="{!! (route_contains('billboard-faces','IN')) ? 'active' : '' !!}">
     <a class="nav-link" href="{{ route('billboard-faces.index') }}">Billboard Faces</a>
 </li>
-<li class="{!! (route_contains('csv-upload', 'IN')) ? 'active' : '' !!}">
-    <a href="{{ route('csv-upload.index') }}">Import CSV File</a>
-</li>
 <li class="{!! (route_contains('clients', 'IN')) ? 'active' : '' !!}">
     <a href="{{ route('clients.index') }}">Clients</a>
 </li>
