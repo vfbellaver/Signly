0dabe8cace6adb510eba377a9f262f49bd62f9f2
Vagner slcdevshop <vfbellaver@outlook.com>
2017-10-04 15:40:43 -0400

    ignore ide_helper

--- resources/assets/js/components/billboard-face/billboard-face-form.vue
+++ resources/assets/js/components/billboard-face/billboard-face-form.vue
@@ -100,14 +100,14 @@
                             <column size="4">
                                 <form-group :form="form" field="lights_on">
                                     <input-label for="lights_on">Lights On At: </input-label>
-                                    <input-time v-model="form.lights_on" id="lights_on" name="lights_on"></input-time>
+                                    <vue-timepicker v-model="lights_on" id="lights_on" name="lights_on"></vue-timepicker>
                                 </form-group>
                             </column>
 
                             <column size="4"">
                                 <form-group :form="form" field="lights_off">
                                     <input-label for="lights_off">Lights Off At: </input-label>
-                                    <input-time v-model="form.lights_off" id="lights_off" name="lights_off"></input-time>
+                                    <vue-timepicker v-model="lights_off" id="lights_off" name="lights_off"></vue-timepicker>
                                 </form-group>
                             </column>
                         </row>
