91585b478542762a518525646d0c78872bf207cd
Daniel Camargo <daniel@slcdevshop.com>
2017-10-05 09:47:49 -0600

    refactor app.js

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,55 +1,48 @@
 require('./vendor');
 
 import Utils from './commons/utils';
+import Layout from './commons/layout';
+import Laroute from './commons/laroute';
+import Vue from 'vue';
+import * as VueGoogleMaps from 'vue2-google-maps';
 
 window.Utils = Utils();
-
-import Layout from './commons/layout';
-
 window.Layout = Layout();
-
-import Laroute from './commons/laroute';
-
 window.laroute = Laroute;
+window.Vue = Vue;
 
-window.Vue = require('vue');
+//window.Bus = new Vue();
 
 require('./directives/bootstrap');
 require('./components/bootstrap');
-
-window.Bus = new Vue();
 require('./vue/bootstrap');
 
-import * as VueGoogleMaps from 'vue2-google-maps';
-
-window.Vue.use(VueGoogleMaps, {
+Vue.use(VueGoogleMaps, {
     load: {
         key: 'AIzaSyC2z5mUPBMQj4xb6VNzX32Iv-5xFzcpxu4',
         libraries: 'places',
     }
 });
 
+window.Bus = new Vue({
+    el: '#app',
 
-const app = new Vue({
-        el: '#app',
+    data() {
+        return {
+            user: 'Slc' in window ? Slc.user : null,
+            isMenuVisible: true,
+        }
+    },
 
-        data() {
-            return {
-                user: 'Slc' in window ? Slc.user : null,
-                isMenuVisible: true,
-            }
-        },
-
-        created() {
-            console.log("App Created");
-            window.Layout.init();
-            let self = this;
-            Bus.$on('loadCurrentUser', () => {
-                axios.get(laroute.route('api.current.user'))
-                    .then(response => {
-                        self.user = response.data;
-                    });
-            });
-        },
-    })
-;
+    created() {
+        console.log("App Created");
+        window.Layout.init();
+        let self = this;
+        Bus.$on('loadCurrentUser', () => {
+            axios.get(laroute.route('api.current.user'))
+                .then(response => {
+                    self.user = response.data;
+                });
+        });
+    },
+});
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@ Route::get('roles', function () {
     dd($roles);
 });
 
+Route::get('slc.js', function () {
+    $json = json_encode(array_merge(Slc::scriptVariables(), []));
+    $js = <<<js
+window.Slc = {$json};
+js;
+    return response($js)->header('Content-Type', 'text/javascript');
+})->name('slc.js');
+
 foreach (File::files(app()->path() . '/Routes/guest') as $file) {
     require $file;
 }
