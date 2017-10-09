5aaa8a1b9c731915f0b4fbfe4d4ae4eca89c20db
Daniel Camargo <daniel@slcdevshop.com>
2017-10-09 10:51:16 -0600

    changing billboard import page

---
+++ resources/assets/js/components/demo/list.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<style>
+
+</style>
+
+<script>
+    import _ from 'lodash';
+    import * as Slc from "../../vue/http";
+
+</script>
--- resources/assets/js/components/shared/Modals/Modal.vue
+++ resources/assets/js/components/shared/Modals/Modal.vue
@@ -1,9 +1,31 @@
 <template>
     <div class="modal inmodal fade" tabindex="-1" role="dialog" aria-hidden="true">
-        <div class="modal-dialog">
+        <div class="modal-dialog" :class="modalSize">
             <div class="modal-content animated bounceInTop">
                 <slot></slot>
             </div>
         </div>
     </div>
 </template>
+
+<script>
+    export default {
+        props: {
+            size: {required: false}
+        },
+        computed: {
+            modalSize() {
+                if (!this.size) {
+                    return "modal-md";
+                }
+                if (this.size === 'sm') {
+                    return "modal-sm";
+                }
+                if (this.size === 'lg') {
+                    return "modal-lg";
+                }
+            }
+        }
+
+    }
+</script>
