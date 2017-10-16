f890d05c11f64a86f6512313513e634cc9670d7e
Vagner slcdevshop <vfbellaver@outlook.com>
2017-10-16 10:10:48 -0300

    #29 table other informations in faces billboard

--- resources/assets/js/components/billboard-public/faces-billboard.vue
+++ resources/assets/js/components/billboard-public/faces-billboard.vue
@@ -1,68 +1,72 @@
 <template>
     <div>
-        <div  v-for="face in billboardFaces">
-                <div class="col-md-6">
-                    <div class="ibox float-e-margins">
-                        <div class="ibox-title">
-                            <h3>Billboard Face Description</h3>
+        <div v-for="face in billboardFaces">
+            <div class="col-md-6">
+                <div class="ibox float-e-margins">
+                    <div class="ibox-title">
+                        <h3>Billboard Face Description</h3>
+                    </div>
+                    <div>
+                        <div class="ibox-content no-padding border-left-right">
+                            <img alt="image" class="img" :src="face.photo" width="100%">
                         </div>
-                        <div>
-                            <div class="ibox-content no-padding border-left-right">
-                                <img alt="image" class="img" :src="face.photo" width="100%">
+
+                        <div class="ibox-content profile-content">
+                            <row>
+                                <h2 class="font-bold m-b-xs col-md-6">
+                                    Billboard Face - {{face.label}}
+                                </h2>
+
+                                <h2 class="font-bold m-b-xs">
+                                    Code - {{face.code}}
+                                </h2>
+                            </row>
+                            <small>{{face.notes}}</small>
+                            <hr>
+                            <div>
+                                <div class="col-md-6">
+                                    <p>Monthly Impressions</p>
+                                    <h1>{{face.monthly_impressions}}</h1>
+                                </div>
+                                <div>
+                                    <p>Hard Cost</p>
+                                    <h1 class="product-main-price">$ {{formatMoney(face.hard_cost)}}</h1>
+                                </div>
                             </div>
-
-                            <div class="ibox-content profile-content">
-
-                                    <h2 class="font-bold m-b-xs">
-                                        Desktop publishing software
-                                    </h2>
-                                    <small>Many desktop publishing packages and web page editors now.</small>
-                                    <hr>
-                                    <div>
-                                        <button class="btn btn-primary pull-right">Add to cart</button>
-                                        <h1 class="product-main-price">$406,602 <small class="text-muted">Exclude Tax</small> </h1>
-                                    </div>
-                                    <hr>
-                                    <h4>Product description</h4>
-
-                                    <div class="small text-muted">
-                                        It is a long established fact that a reader will be distracted by the readable
-                                        content of a page when looking at its layout. The point of using Lorem Ipsum is
-                                        that it has a more-or-less normal distribution of letters, as opposed to using
-                                        'Content here, content here', making it look like readable English.
-                                        <br>
-                                        <br>
-                                        There are many variations of passages of Lorem Ipsum available, but the majority
-                                        have suffered alteration in some form, by injected humour, or randomised words
-                                        which don't look even slightly believable.
-                                    </div>
-                                    <dl class="dl-horizontal m-t-md small">
-                                        <dt>Description lists</dt>
-                                        <dd>A description list is perfect for defining terms.</dd>
-                                        <dt>Euismod</dt>
-                                        <dd>Vestibulum id ligula porta felis euismod semper eget lacinia odio sem nec elit.</dd>
-                                        <dd>Donec id elit non mi porta gravida at eget metus.</dd>
-                                        <dt>Malesuada porta</dt>
-                                        <dd>Etiam porta sem malesuada magna mollis euismod.</dd>
-                                        <dt>Felis euismod semper eget</dt>
-                                        <dd>Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</dd>
-                                    </dl>
-                                    <div class="text-right">
-                                        <div class="btn-group">
-                                            <button class="btn btn-white btn-sm"><i class="fa fa-star"></i> Add to wishlist </button>
-                                            <button class="btn btn-white btn-sm"><i class="fa fa-envelope"></i> Contact with author </button>
-                                        </div>
-                                    </div>
-
-                            </div>
+                            <hr>
+                            <table class="table">
+                                <thead>
+                                <tr>
+                                    <th>Width</th>
+                                    <th>Height</th>
+                                    <th>Type</th>
+                                    <th>Duration</th>
+                                    <th>Illuminated</th>
+                                    <th>Lights on</th>
+                                    <th>Lights off</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr>
+                                    <td>{{face.width}}</td>
+                                    <td>{{face.height}}</td>
+                                    <td>{{face.type}}</td>
+                                    <td>{{face.duration}}</td>
+                                    <td>{{face.is_illuminated}}</td>
+                                    <td>{{face.lights_on}}</td>
+                                    <td>{{face.lights_off}}</td>
+                                </tr>
+                                </tbody>
+                            </table>
                         </div>
                     </div>
                 </div>
+            </div>
 
 
         </div>
 
-   </div>
+    </div>
 </template>
 <style lang="scss" scoped="scoped">
     .img {
@@ -104,6 +108,25 @@
                         self.billboardFaces = response;
                     });
             },
+
+            formatMoney(money){
+
+                let v = money;
+                v = v.toString().replace(/[^0-9]/g, "");
+                if (v === undefined || v === null || v.length === 0) {
+                    return "";
+                }
+
+                v = v.replace(/^0*/g, "");
+                v = v.replace(/^$/, "0.00");
+                v = v.replace(/^(\d)$/, "0.0$1");
+                v = v.replace(/^(\d{2})$/, "0.$1");
+                v = v.replace(/(\d+)(\d{2})$/, "$1.$2");
+                for (let i = 0; i < 10; i++) {
+                    v = v.replace(/(\d)(\d{3}[.,])/, "$1,$2");
+                }
+                return v;
+            }
 
         }
     }
