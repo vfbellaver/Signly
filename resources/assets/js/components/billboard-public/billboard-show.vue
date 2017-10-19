5f9f1fc6d590ee055dea5f57b3c01166153d67d7
Vagner slcdevshop <vfbellaver@outlook.com>
2017-10-19 17:43:10 -0300

    #29 fixing view interface

--- resources/assets/js/components/billboard-public/billboard-show.vue
+++ resources/assets/js/components/billboard-public/billboard-show.vue
@@ -4,22 +4,64 @@
             <div class="container-fluid">
                 <box>
                     <box-content>
-                        <h2 align="center">Billboard Information</h2>
+                        <h1 class=" text-center font-bold m-b-xs">
+                            Billboard Information
+                        </h1>
                         <hr/>
                         <row>
-                            <column size="6">
-                                <h4>Name</h4>
-                                <p class="name">{{billboard.name}}</p>
+                            <column size="4">
+                                <h2 class="font-bold m-b-xs">
+                                    Team
+                                </h2>
+                                <hr>
+                                <div class="contact-box">
+                                        <div class="col-sm-6">
+                                            <div class="text-center">
+                                                <img alt="image" class="img-circle m-t-xs img-responsive" src="">
+                                                <div class="m-t-xs font-bold">Graphics designer</div>
+                                            </div>
+                                        </div>
+                                        <div class="col-sm-6">
+                                            <h3><strong>John Smith</strong></h3>
+                                            <p><i class="fa fa-map-marker"></i> Riviera State 32/106</p>
+                                            <address>
+                                                <strong>Twitter, Inc.</strong><br>
+                                                795 Folsom Ave, Suite 600<br>
+                                                San Francisco, CA 94107<br>
+                                                <abbr title="Phone">P:</abbr> (123) 456-7890
+                                            </address>
+                                        </div>
+                                        <div class="clearfix"></div>
+                                    </a>
+                                </div>
                             </column>
-                            <column size="6">
-                                <h4>Description</h4>
-                                <p class="description">{{billboard.description}}</p>
+                            <column size="8">
+
+
+                                    <h2 class="font-bold m-b-xs">
+                                        {{billboard.name}}
+                                    </h2>
+                                    <hr>
+
+                                    <h3>Billboard description</h3>
+
+                                    <div class="text-muted">
+                                        It is a long established fact that a reader will be distracted by the readable
+                                        content of a page when looking at its layout. The point of using Lorem Ipsum is
+                                        There are many variations of passages of Lorem Ipsum available, but the majority
+                                        have suffered alteration in some form, by injected humour, or randomised words
+                                        which don't look even slightly believable.
+                                    </div>
+                                    <dl class="m-t-md">
+                                        <dt>Description lists</dt>
+                                        <dd>A description list is perfect for defining terms.</dd>
+                                        <dt>Euismod</dt>
+                                    </dl>
+
                             </column>
                         </row>
-                        <hr/>
                         <div class="row">
-                            <div class="col-md-6">
-                                <h4>Localization in Map</h4>
+                            <div class="col-md-12">
                                 <hr/>
                                 <gmap-map
                                         v-if="loaded"
@@ -27,7 +69,7 @@
                                         :zoom="zoom"
                                         @zoom_changed="onZoomChanged"
                                         :options="mapOptions"
-                                        style="width: 100%; min-height: 360px;">
+                                        style="width: 100%; min-height: 500px;">
                                     <gmap-marker
                                             v-if="marker"
                                             :position="marker"
@@ -35,8 +77,7 @@
                                     ></gmap-marker>
                                 </gmap-map>
                             </div>
-                            <div class="col-md-6">
-                                <h4>Street View</h4>
+                            <div class="col-md-12">
                                 <hr/>
                                 <gmap-street-view-panorama
                                         v-if="loaded"
@@ -70,7 +111,7 @@
     }
 
     .vue-street-view-pano-container {
-        min-height: 360px;
+        min-height: 500px;
     }
 
     .vue-map-container .map-vue {
