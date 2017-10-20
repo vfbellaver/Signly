97e20f79c74aaa8848e2882defebb01de42c1d73
rodrigoslcdevshop <rodrigcursino@gmail.com>
2017-10-20 16:53:14 -0300

    #13 create view for signing

---
+++ resources/assets/js/components/payment/content-plan.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <div class="col-lg-4">
+            <div class="register-box">
+                <div class="register-circulo">
+                    <i class="fa fa-handshake-o" aria-hidden="true"></i>
+                </div>
+                <hr>
+                <div class="text-center">
+                    <h1>
+                        <small>PLAN</small>
+                    </h1>
+                    <h2>{{name}}</h2>
+
+                </div>
+            </div>
+            <div class="register-footer">
+                <h1>{{value}}</h1>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+       props:{
+           name:{required:true},
+           value:{required:true},
+       }
+    }
+</script>
---
+++ resources/assets/js/components/payment/plan.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <div class="col-lg-4">
+            <div class="register-box">
+                <div class="register-circulo">
+                    <i class="fa fa-handshake-o" aria-hidden="true"></i>
+                </div>
+                <hr>
+                <div class="text-center">
+                    <h1>
+                        <small>PLAN</small>
+                    </h1>
+                    <h2>ENTERPRISE TEAM
+
+                </div>
+            </div>
+            <div class="register-footer">
+                <h1>$499</h1>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
