9bb264ea65232323ccb237634f9a8a83810a9bc1
rodrigoslcdevshop <rodrigcursino@gmail.com>
2017-10-30 14:01:28 -0300

    #13 subscription delete

---
+++ app/Http/Requests/PaymentRegistrationRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserRegistrationRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'password' => 'required|string|min:6|confirmed',
+            'email' => 'required|email|unique:users,email',
+            'team' => 'required|unique:teams,name',
+            'plan' => 'required',
+        ];
+    }
+}
---
+++ resources/views/components/payment-page.blade.php
@@ -0,0 +1,9 @@
+<div class="wrapper wrapper-content">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                {{ $slot }}
+            </div>
+        </div>
+    </div>
+</div>
