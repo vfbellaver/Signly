4acfde751f20c56aa094ed0491a853a60e178ccb
Daniel Camargo <daniel@slcdevshop.com>
2017-10-30 16:52:10 -0600

    #36 fixing public view billboard

--- resources/views/billboard/public.blade.php
+++ resources/views/billboard/public.blade.php
@@ -3,7 +3,16 @@
 @section('base-content')
 
     <div id="public-billboard">
-
+        <div class="billboard-header">
+            {{$billboard->name}}
+        </div>
+        <hr/>
+        <div class="billboard-faces">
+            @foreach($billboard->billboardFaces as $face)
+                <div class="billboard-face">
+                    {{$face->code}} - {{$face->label}}
+                </div>
+            @endforeach
+        </div>
     </div>
-
 @endsection
