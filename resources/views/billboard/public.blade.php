ca05d352ab1639dcb7d7b8cf83c946f01acd6311
Vagner slcdevshop <vfbellaver@outlook.com>
2017-11-01 09:49:47 -0300

    #36 adjust query public view, adjust header public page email and address owner company

--- resources/views/billboard/public.blade.php
+++ resources/views/billboard/public.blade.php
@@ -11,7 +11,7 @@
             </div>
             <div class="col-md-4">
                 <strong> <i class="fa fa-send"></i> Email:</strong><br>
-                {{$billboard->name}}
+                {{$owner->email}}
             </div>
             <div class="col-md-3">
                 <strong> <i class="fa fa-phone"></i> Phones:</strong><br>
@@ -20,18 +20,18 @@
             <div class="col-md-5">
                 <address>
                     <strong> <i class="fa fa-map-marker"></i> Address:</strong><br>
-                    {{$billboard->address}}
+                    {{$owner->address}}
                 </address>
             </div>
         </div>
         <div class="col-md-12">
-            <hr style="border: 1px solid #00A5E3"/>
+            <hr style="border-bottom: 2px solid #00A5E3"/>
         </div>
         <div class="col-md-6">
             <gmap-map
                     :center="{{json_encode(['lat'=> $billboard->lat,'lng'=>$billboard->lng])}}"
                     :zoom="7"
-                    style="width: 95%; min-height: 200px">
+                    style="width: 95%; min-height: 185px">
                 <gmap-marker
                         :position="{{json_encode(['lat'=> $billboard->lat,'lng'=>$billboard->lng])}}"
                         :icon="{{
@@ -57,49 +57,50 @@
         </div>
 
         <div class="col-md-12">
-            <hr style="border: 1px solid #00A5E3"/>
+            <hr style="border-bottom: 2px solid #00A5E3"/>
         </div>
-        <div class="col-md-12 text-center" >
-        <h3 style="margin-top: 0">Billboard Faces</h3>
+
+        <!-- BILLBOARD FACES -->
+        <div class="col-md-12 text-center">
+            <h4 style="margin-top: 0">Billboard Faces</h4>
         </div>
         @foreach($billboard->billboardFaces as $face)
             <div class="billboard-face col-md-6">
-                <div>
-                    <div class="col-md-5">
-                        <div class="text-center">
-                            <img alt="Face" src="{{$face->photo}}" width="100%">
-                        </div>
+                <hr style="border-bottom: 2px solid #00A5E3; margin-top: 0"/>
+                <div class="col-md-5">
+                    <div class="text-center">
+                        <img alt="Face" src="{{$face->photo}}" width="100%">
                     </div>
-                    <div class="col-md-7">
-                        <h5><strong> {{$face->code}} - {{$face->label}}</strong></h5>
-                        <p><i class="fa fa-map-marker"></i> Riviera State 32/106</p>
-                    </div>
-                    <div class="col-md-12">
-                        <table class="table">
-                            <thead style="font-size: 10px">
-                            <tr>
-                                <th>Type</th>
-                                <th>Illuminated</th>
-                                <th>Lights on</th>
-                                <th>Lights off</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            <tr>
+                </div>
+                <div class="col-md-7">
+                    <h5><strong> {{$face->code}} - {{$face->label}}</strong></h5>
+                    <small><strong>Hard Cost:</strong></small>
+                    <p><i class="fa fa-dollar"></i> {{$face->hard_cost}}</p>
+                </div>
+                <div class="col-md-12">
+                    <table class="table">
+                        <thead style="font-size: 10px">
+                        <tr>
+                            <th>Type</th>
+                            <th>Illuminated</th>
+                            <th>Lights on</th>
+                            <th>Lights off</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
 
-                                <td>{{$face->type}}</td>
-                                <td>{{($face->is_illuminated ? 'Yes' : 'No')}}</td>
-                                <td>{{$face->lights_on}}</td>
-                                <td>{{$face->lights_off}}</td>
-                            </tr>
-                            </tbody>
-                        </table>
-                    </div>
+                            <td>{{$face->type}}</td>
+                            <td>{{($face->is_illuminated ? 'Yes' : 'No')}}</td>
+                            <td>{{$face->lights_on}}</td>
+                            <td>{{$face->lights_off}}</td>
+                        </tr>
+                        </tbody>
+                    </table>
                 </div>
 
             </div>
         @endforeach
     </div>
-
 @endsection
 
