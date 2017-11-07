89567ce865a43abf303196e227e415002c040559
Vagner slcdevshop <vfbellaver@outlook.com>
2017-11-07 16:21:36 -0300

    #36 adjust faces public page

--- resources/views/billboard/public.blade.php
+++ resources/views/billboard/public.blade.php
@@ -27,65 +27,79 @@
         <div class="col-md-12">
             <hr/>
         </div>
-
-            <div class="col-md-6">
-                <img width="96%" class="text-center" src="/images/pov_img.png">
-            </div>
-            <div class="col-md-6">
-                <h3 class="text-center"><strong>{{$billboard->name}}</strong></h3>
-                <address>
-                    <strong> <i class="fa fa-map-marker"></i> Address:</strong><br>
-                    {{$billboard->address}}
-                </address>
-                <strong> <i class="fa fa-edit"></i> Description:</strong><br>
-                <p class="text-justify">{{$billboard->description}}</p>
-            </div>
+        <div class="col-md-6">
+            <img width="96%" class="text-center" src="/images/pov_img.png">
+        </div>
+        <div class="col-md-6">
+            <h3 class="text-center"><strong>{{$billboard->name}}</strong></h3>
+            <address>
+                <strong> <i class="fa fa-map-marker"></i> Address:</strong><br>
+                {{$billboard->address}}
+            </address>
+            <strong> <i class="fa fa-edit"></i> Description:</strong><br>
+            <p class="text-justify">{{$billboard->description}}</p>
+        </div>
 
 
         <div class="col-md-12">
             <hr/>
+            <h3 class="text-center">Billboard Faces</h3>
+            <hr/>
         </div>
 
         <!-- BILLBOARD FACES -->
-        <h3 class="text-center">Billboard Faces</h3>
-        @foreach($billboard->billboardFaces as $face)
 
+        @foreach($billboard->billboardFaces as $face)
             <div class="row col-md-12">
-                <hr/>
                 <div class="col-md-6">
                     <img alt="Face" class="img-responsive" src="{{$face->photo}}" width="100%">
                 </div>
                 <div class="col-md-6">
-                    <p> {{$face->code}}</p>
-                    <p> {{$face->label}}</p>
-                    <p> {{$face->hard_cost}}</p>
-                    <p> {{$face->duration}}</p>
-                    <p> {{$face->height}}</p>
-                    <p> {{$face->width}}</p>
-                    <p> {{$face->reads}}</p>
-                    <p> {{$face->notes}}</p>
-                    <p> {{$face->monthly_impressions}}</p>
-                    <p> {{$face->max_ads}}</p>
-                    <table class="table">
-                        <thead style="font-size: 10px">
-                        <tr>
-                            <th>Illuminated</th>
-                            <th>Lights on</th>
-                            <th>Lights off</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <tr>
-                            <td>{{($face->is_illuminated ? 'Yes' : 'No')}}</td>
-                            <td>{{$face->lights_on}}</td>
-                            <td>{{$face->lights_off}}</td>
-                        </tr>
-                        </tbody>
-                    </table>
+                    <div class="label-inline">
+                        <h3>Billboard Face {{$face->label}}</h3>
+                    </div>
+                    <div class="ibox-content">
+                        <div class="row  m-t-sm">
+                            <div class="col-sm-6">
+                                <p>Hard Cost</p>
+                                <h2 class="font-bold">$ {{$face->hard_cost}}</h2>
+                            </div>
+                            <div class="col-sm-6">
+                                <p>Montlhy Impressions</p>
+                                <h2 class="font-bold">{{$face->monthly_impressions}}</h2>
+                            </div>
+                        </div>
+
+
+                        <hr style="margin-top: 5px; margin-bottom: 5px">
+                        <table class="table">
+                            <thead>
+                            <tr>
+                                <th>Illuminated</th>
+                                <th>Lights on</th>
+                                <th>Lights off</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td>{{($face->is_illuminated ? 'Yes' : 'No')}}</td>
+                                <td>{{$face->lights_on}}</td>
+                                <td>{{$face->lights_off}}</td>
+                            </tr>
+                            </tbody>
+                        </table>
+
+                    </div>
                 </div>
-
             </div>
-
+            <div class="col-md-12">
+                <div class="well m-t"><strong>Note:</strong>
+                    {{$face->notes}}
+                </div>
+            </div>
+            <div class="col-md-12">
+                <hr/>
+            </div>
         @endforeach
         <div style="clear: both"></div>
     </div>
