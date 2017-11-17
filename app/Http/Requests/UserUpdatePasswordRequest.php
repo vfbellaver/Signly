32bb21714234b888c1420cec68e0a966944c8135
Rafael Lunardelli <rafael@slcdevshop.com>
2017-11-17 14:05:05 -0700

    adding feedback to the user on profile settings forms

--- app/Http/Requests/UserUpdatePasswordRequest.php
+++ app/Http/Requests/UserUpdatePasswordRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use App\Rules\CurrentPassword;
+use App\Rules\PasswordFormat;
 use Illuminate\Foundation\Http\FormRequest;
 
 class UserUpdatePasswordRequest extends FormRequest
@@ -16,7 +17,7 @@ class UserUpdatePasswordRequest extends FormRequest
     {
         return [
             'current_password' => ['required', new CurrentPassword],
-            'new_password' => 'required|min:6',
+            'new_password' => ['required', new PasswordFormat],
             'confirm_password' => 'required|same:new_password'
         ];
     }
