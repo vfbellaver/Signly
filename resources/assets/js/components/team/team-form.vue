9f8b148af38c6faf6e017ec3979b5c5b18fd1fc0
Rafael Lunardelli <rafael@slcdevshop.com>
2017-11-17 14:17:46 -0700

    adding feedback to the user on company settings

--- resources/assets/js/components/team/team-form.vue
+++ resources/assets/js/components/team/team-form.vue
@@ -5,11 +5,11 @@
                 <h5>Company Logo</h5>
             </div>
             <div class="ibox-content">
-                <form-submit v-model="team" @submit="saveTeamLogo" class="wizard-big wizard clearfix">
+                <form-submit v-model="formLogo" @submit="saveTeamLogo" class="wizard-big wizard clearfix">
                     <row>
                         <div class="col-lg-4 col-sm-4 col-xs-12">
-                            <form-group width="256px" :form="team" field="logo">
-                                <image-upload v-model="team.logo" id="logo"
+                            <form-group width="256px" :form="formLogo" field="logo">
+                                <image-upload v-model="formLogo.logo" id="logo"
                                               name="logo"></image-upload>
                             </form-group>
                         </div>
@@ -17,7 +17,9 @@
                     <row>
                         <div class="col-xs-12">
                             <hr>
-                            <button type="submit" class="btn btn-primary">Update Your Logo
+                            <button type="submit" class="btn btn-primary" :disabled="formLogo.busy">
+                                <spinner v-if="formLogo.busy"></spinner>
+                                Update Your Logo
                             </button>
                         </div>
                         <div class="clear"></div>
@@ -30,36 +32,36 @@
                 <h5>Company Settings</h5>
             </div>
             <div class="ibox-content">
-                <form-submit v-model="team" @submit="saveTeamName" class="wizard-big wizard clearfix">
+                <form-submit v-model="form" @submit="saveTeamName" class="wizard-big wizard clearfix">
                     <row>
                         <div class="col-lg-12">
-                            <form-group :form="team" field="name">
+                            <form-group :form="form" field="name">
                                 <input-label for="name">Name: </input-label>
-                                <input-text v-model="team.name" id="name"
+                                <input-text v-model="form.name" id="name"
                                             name="name"></input-text>
                             </form-group>
-                            <form-group :form="team" field="email">
+                            <form-group :form="form" field="email">
                                 <input-label for="email">Email: </input-label>
-                                <input-text v-model="team.email" id="email"
+                                <input-text v-model="form.email" id="email"
                                             name="email"></input-text>
                             </form-group>
-                            <form-group :form="team" field="address">
+                            <form-group :form="form" field="address">
                                 <input-label for="address">Address: </input-label>
-                                <input-text v-model="team.address" id="address"
+                                <input-text v-model="form.address" id="address"
                                             name="address"></input-text>
                             </form-group>
                             <row>
                                 <column size="6">
-                                    <form-group :form="team" field="phone">
+                                    <form-group :form="form" field="phone">
                                         <input-label for="phone">Phone: </input-label>
-                                        <input-text v-model="team.phone" id="phone"
+                                        <input-text v-model="form.phone" id="phone"
                                                     name="phone" v-tel></input-text>
                                     </form-group>
                                 </column>
                                 <column size="6">
-                                    <form-group :form="team" field="fax">
+                                    <form-group :form="form" field="fax">
                                         <input-label for="fax">Fax: </input-label>
-                                        <input-text v-model="team.fax" id="fax"
+                                        <input-text v-model="form.fax" id="fax"
                                                     name="fax" placeholder="(Optional)" v-tel></input-text>
                                     </form-group>
                                 </column>
@@ -67,7 +69,9 @@
 
                             <hr>
                             <div>
-                                <button type="submit" class="btn btn-primary">Update Company Settings
+                                <button type="submit" class="btn btn-primary" :disabled="form.busy">
+                                    <spinner v-if="form.busy"></spinner>
+                                    Update Company Settings
                                 </button>
                             </div>
                         </div>
@@ -115,40 +119,43 @@
         data() {
             return {
                 team: Slc.user.team,
+                form: {},
+                formLogo: {}
             }
         },
 
-        created(){
-            this.buildForm();
+        created() {
+            this.form = new SlcForm({
+                id: this.team.id,
+                name: this.team.name,
+                email: this.team.email,
+                phone: this.team.phone,
+                address: this.team.address,
+                fax: this.team.fax,
+                logo: this.team.logo ? this.team.logo : null,
+            });
+
+            this.formLogo = new SlcForm({
+                id: this.team.id,
+                logo: this.team.logo ? this.team.logo : null,
+            });
         },
 
         methods: {
 
-            saveTeamName(){
+            saveTeamName() {
                 const self = this;
                 const uri = laroute.route('api.team.update.name', {team: this.team.id});
-                SLC.put(uri, self.team).then((response) => {
+                SLC.put(uri, self.form).then((response) => {
                     console.log('Team updated ', response);
                 });
             },
 
-            saveTeamLogo(){
+            saveTeamLogo() {
                 const self = this;
                 const uri = laroute.route('api.team.update.logo', {team: this.team.id});
-                SLC.put(uri, self.team).then((response) => {
+                SLC.put(uri, self.formLogo).then((response) => {
                     console.log('Team updated ', response);
-                });
-            },
-
-            buildForm(){
-                this.team = new SlcForm({
-                    id: this.team.id,
-                    name: this.team.name,
-                    email: this.team.email,
-                    phone: this.team.phone,
-                    address: this.team.address,
-                    fax: this.team.fax,
-                    logo: this.team.logo ? this.team.logo : null,
                 });
             },
         }
