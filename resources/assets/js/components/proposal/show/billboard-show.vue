8fe2b84e4bed4991ef829b369a79a767b8bb8565
Daniel Camargo <daniel@slcdevshop.com>
2017-11-17 16:07:36 -0700

    #36 show billboard fixes

--- resources/assets/js/components/proposal/show/billboard-show.vue
+++ resources/assets/js/components/proposal/show/billboard-show.vue
@@ -2,16 +2,43 @@
     <div class="info-window">
         <tabs>
             <tab :key="face.id" v-for="(face, i) in billboard.billboard_faces" :name="face.label" :selected="i == 0">
-                <h3>{{face.code}}</h3>
-                <div class="row">
-                    <div class="col-xs-4 no-padding">
-                        <img class="img-responsive" :src="face.photo_url" alt="label"/>
+                <div style="padding: 0 15px;">
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <strong style="position: relative; top: 4px;">{{face.code}}</strong>
+                            <img alt="image" class="pull-right" style="max-width: 128px" :src="face.photo_url">
+                        </div>
                     </div>
-                    <div class="col-xs-7">
+                    <div class="hr-line-dashed"></div>
+                    <div class="row" style="height: 82px;">
+                        <div class="col-xs-12">
+                            <ul class="list-group clear-list">
+                                <li class="list-group-item fist-item">
+                                    <span class="pull-right">{{billboard.address}}</span>
+                                    Address
+                                </li>
+                                <li class="list-group-item">
+                                    <span class="pull-right">{{face.type}}</span>
+                                    Type
+                                </li>
+                                <li class="list-group-item">
+                                    <span class="pull-right">{{face.reads}}</span>
+                                    Reads
+                                </li>
+                                <li v-if="face.width && face.height" class="list-group-item">
+                                    <span class="pull-right">{{face.width}} x {{face.height}}</span>
+                                    Dimension
+                                </li>
+                                <li class="list-group-item">
+                                    <span class="pull-right">{{face.rate_card | money('$')}}</span>
+                                    Rate Card
+                                </li>
+                            </ul>
 
-                    </div>
-                    <div>
-                        <button class="btn btn-primary" @click="add(face)" :disabled="disabled(face)">Add</button>
+                            <button class="btn btn-primary btn-sm" @click="add(face)" :disabled="disabled(face)">
+                                Add to Proposal
+                            </button>
+                        </div>
                     </div>
                 </div>
             </tab>
@@ -27,21 +54,6 @@
 
         .panel-body {
             min-height: 270px;
-        }
-
-        .description {
-            text-align: justify;
-        }
-        .img-responsive {
-            max-width: 100%;
-        }
-        .dl-horizontal {
-            dt {
-                width: 80px;
-            }
-            dd {
-                margin-left: 92px;
-            }
         }
     }
 </style>
